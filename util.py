240fba4af1a05ebc129c0741fcfcade2baa6ea7c
foril <1571825323@qq.com>
2023-12-12 21:25:29 +0800

    feat: 过滤空白符冲突以及 crlf 冲突

--- util.py
+++ util.py
@@ -9,3 +9,16 @@ class Conflict:
         self.base = base
         self.resolution = resolution
         self.resolution_kind = resolution_kind
+
+def conflict2file(conflict: Conflict, path: Path):
+    def list2line(list: List[str]):
+        return '\n'.join(list)
+    with open(path / 'base.txt', 'w') as f:
+        f.write(list2line(conflict.base))
+    with open(path / 'ours.txt', 'w') as f:
+        f.write(list2line(conflict.ours))
+    with open(path / 'theirs.txt', 'w') as f:
+        f.write(list2line(conflict.theirs))
+    if conflict.resolution is not None:
+        with open(path / 'resolution.txt', 'w') as f:
+            f.write(list2line(conflict.resolution))
