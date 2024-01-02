ad8c90458412a01a63ca34bbbfc344426489a0cc
foril <1571825323@qq.com>
2024-01-02 20:07:30 +0800

    chore

--- util/tokenizer_util.py
+++ util/tokenizer_util.py
@@ -1,13 +1,14 @@
 # Load model directly
+from typing import List
 from transformers import AutoTokenizer, AutoModelForMaskedLM
 
 tokenizer = AutoTokenizer.from_pretrained("huggingface/CodeBERTa-small-v1")
 
-def encode(text: str):
+def encode(text: str) -> list[int]:
     return tokenizer.encode(text)
 
-def decode(token_ids: list[int]):
-    return tokenizer.convert_ids_to_tokens(token_ids)
+def decode(token_ids: list[int]) -> List[str]:
+    return tokenizer.convert_ids_to_tokens(token_ids) # type: ignore
 
 if __name__ == '__main__':
     output = encode('\t\t\t @Parameters(commandDescription = "Frequency count a structured input instance file.")\n   \t ')
